6926e9f9e505fda681749762522997a21ce87c90
yesterdayskptv <rdunevant@hotmail.com>
2025-08-23 10:49:47 -0700

    scrapbook format update

--- _vti_cnf/animate.js
+++ _vti_cnf/animate.js
@@ -6,4 +6,4 @@ vti_modifiedby:SR|RON-AMD7700X-PC\\rdune
 vti_timecreated:TR|28 Nov 2004 19:59:00 -0000
 vti_cacheddtm:TX|28 Nov 2004 19:59:00 -0000
 vti_filesize:IR|14261
-vti_backlinkinfo:VX|localtemplate.html
+vti_backlinkinfo:VX|localtemplate.html scrapbook/scrapbook00s.html
